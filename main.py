1fb67ef19c283e5ccd1e207e3dcf23f67670fdb4
Braian <braianyachuk@gmail.com>
2026-04-14 15:44:31 -0300

    Progreso 1

--- main.py
+++ main.py
@@ -1,67 +1,99 @@
 # (C) Copyright Peter Hinch 2017-2019.
 # Released under the MIT licence.
 
-# This demo publishes to topic "result" and also subscribes to that topic.
-# This demonstrates bidirectional TLS communication.
-# You can also run the following on a PC to verify:
-# mosquitto_sub -h test.mosquitto.org -t result
-# To get mosquitto_sub to use a secure connection use this, offered by @gmrza:
-# mosquitto_sub -h <my local mosquitto server> -t result -u <username> -P <password> -p 8883
-
-# Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
-
-# red LED: ON == WiFi fail
-# green LED heartbeat: demonstrates scheduler is running.
-
 from mqtt_as import MQTTClient
 from mqtt_local import config
 import uasyncio as asyncio
 import dht, machine
 
+# Configuración del sensor y del LED integrado de la Pico W / Pico 2W
 d = dht.DHT22(machine.Pin(15))
 
-def sub_cb(topic, msg, retained):
-    print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
+mensajes_recibidos = []
+evento_mensaje = asyncio.Event()
 
+# Esta es la función que RECIBE los mensajes de MQTTX
+def sub_cb(topic, msg, retained):
+    # Decodificamos los bytes a texto para poder compararlos
+    """topic_str = topic.decode()
+    msg_str = msg.decode()"""
+    mensajes_recibidos.append((topic.decode(), msg.decode()))
+    evento_mensaje.set()
+    #print('Mensaje recibido - Topic: {} -> Valor: {}'.format(topic_str, msg_str))
+    
 async def wifi_han(state):
     print('Wifi is ', 'up' if state else 'down')
     await asyncio.sleep(1)
 
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
+# Al conectar, nos suscribimos a los tópicos que queremos escuchar
 async def conn_han(client):
-    await client.subscribe('ID_DEL_DISPOSITIVO_BRAIAN/temperatura', 1)
-    await client.subscribe('ID_DEL_DISPOSITIVO_BRAIAN/humedad', 1)
+    # Mantenemos las suscripciones anteriores (opcional si solo publicas ahí)
+    #await client.subscribe('ID_DEL_DISPOSITIVO_BRAIAN/temperatura', 1)
+    #await client.subscribe('ID_DEL_DISPOSITIVO_BRAIAN/humedad', 1)
+    
+    # NUEVO: Nos suscribimos al tópico de control para recibir comandos
+    await client.subscribe('ID_DEL_DISPOSITIVO_BRAIAN/control', 1)
 
 async def main(client):
     await client.connect()
-    n = 0
-    await asyncio.sleep(2)  # Give broker time
+    await asyncio.sleep(2)  # Damos tiempo al broker
+    
+    while True:
+        # El programa se queda aquí esperando a que el evento se active
+        await evento_mensaje.wait()
+        
+        # Como el evento se activó, procesamos todos los mensajes en la lista
+        while mensajes_recibidos:
+            # Sacamos el primer mensaje que llegó a la lista
+            topic, msg = mensajes_recibidos.pop(0)
+            
+            print('Procesando en el main - Tópico: {} -> Valor: {}'.format(topic, msg))
+            
+            # ¡Tu código secuencial va aquí!
+            if msg == 'ON':
+                print("Ejecutando ON...")
+                await asyncio.sleep(1) # Puedes usar sleep sin problemas
+                
+            elif msg == 'OFF':
+                print("Ejecutando OFF...")
+        
+        # Una vez que procesamos todo, limpiamos el evento para volver a dormir y esperar
+        evento_mensaje.clear()
+
+
+    """await client.connect()
+    await asyncio.sleep(2)  # Damos tiempo al broker
+    
     while True:
         try:
             d.measure()
             try:
-                temperatura=d.temperature()
+                temperatura = d.temperature()
                 await client.publish('ID_DEL_DISPOSITIVO_BRAIAN/temperatura', '{}'.format(temperatura), qos = 1)
             except OSError as e:
-                print("sin sensor temperatura")
+                print("Error: sin sensor temperatura")
+                
             try:
-                humedad=d.humidity()
+                humedad = d.humidity()
                 await client.publish('ID_DEL_DISPOSITIVO_BRAIAN/humedad', '{}'.format(humedad), qos = 1)
             except OSError as e:
-                print("sin sensor humedad")
+                print("Error: sin sensor humedad")
+                
         except OSError as e:
-            print("sin sensor")
-        await asyncio.sleep(20)  # Broker is slow
-
-# Define configuration
+            print("Error: sin sensor")
+            
+        await asyncio.sleep(20)  # Publicamos cada 20 segundos
+"""
+# Define configuración
 config['subs_cb'] = sub_cb
 config['connect_coro'] = conn_han
 config['wifi_coro'] = wifi_han
 config['ssl'] = True
 
 # Set up client
-MQTTClient.DEBUG = True  # Optional
+MQTTClient.DEBUG = True
 client = MQTTClient(config)
+
 try:
     asyncio.run(main(client))
 finally:
